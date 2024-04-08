abf165141e8ef7ad6fee7f64babbf0a0085022e2
Projit Biswas <projit.biswas99@gmail.com>
2024-04-08 15:55:45 +0530

    client section complete

--- src/component/Homepage/OurClient.jsx
+++ src/component/Homepage/OurClient.jsx
@@ -5,12 +5,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function OurClient() {
   return (
     <>
-      <div className="bg-black w-full h-full text-white font-mono ">
+      <div className="bg-black w-full h-dvh text-white font-mono ">
         <div className="max-w-[1140px] mx-auto">
 
 
           <div className="w-[620px] mb-[70px] text-left">
-            <h1 className="text-[12px] pl-16 tracking-wide text-left mb-5 opacity-70">
+            <h1 className="text-[12px] pl-16 pt-10 tracking-wider font-semibold text-left mb-3 opacity-70">
               OUR CLIENTS -CLICK ON (+) TO VISIT WEBSITES
             </h1>
             <h1 className="text-[42px] font-bold leading-tight tracking-[0.01em]">Your success, our reputation!</h1>
@@ -23,7 +23,7 @@ function OurClient() {
                 className="max-w-[180px] m-auto h-auto"
                 src="src\assets\moment-300x300.png" alt="" />
                 <div className=" absolute h-[100%] left-0 top-0 w-full ">
-                    <div className="rounded-r-[20px] bottom-0 inline-block h-[40px] w-[40px] left-0 absolute">
+                    <div className="rounded-tr-[20px] bottom-0 inline-block h-[40px] w-[40px] -left-[129%] top-[84%] absolute">
                         <div className="text-[10px] relative top-[50%] cursor-pointer">
                         <FontAwesomeIcon icon="fa-solid fa-plus" style={{color: "#ffffff",}} />
                         </div>
@@ -31,15 +31,62 @@ function OurClient() {
 
                 </div>
             </div>
+
+
+
+
             <div className="bg-[#090909] w-[367px] flex text-center items-center p-[15px] min-h-60 mr-[2px] mb-[2px] overflow-hidden">
                 <img 
                 className="max-w-[180px] m-auto h-auto"
-                src="src\assets\moment-300x300.png" alt="" />
+                src="src\assets\Dream-Nice-300x300.png" alt="" />
+            </div>
+
+
+
+
+
+            <div className="bg-[#090909] w-[367px] flex text-center items-center p-[15px] min-h-60 mr-[2px] mb-[2px] overflow-hidden">
+                <img 
+                className="max-w-[180px] m-auto h-auto"
+                src="src\assets\samarth-300x300.png" alt="" />
+            </div>
+
+
+            <div className="bg-[#090909] w-[367px] flex text-center items-center p-[15px] min-h-60 mr-[2px] mb-[2px] overflow-hidden">
+                <img 
+                className="max-w-[180px] m-auto h-auto"
+                src="src\assets\global-exporter-300x300.png" alt="" />
+            </div>
+
+
+            <div className="bg-[#090909] w-[367px] flex text-center items-center p-[15px] min-h-60 mr-[2px] mb-[2px] overflow-hidden">
+                <img 
+                className="max-w-[180px] m-auto h-auto"
+                src="src\assets\Peakenza-300x300.png" alt="" />
+            </div>
+
+
+
+
+            <div className="bg-[#090909] w-[367px] flex text-center items-center p-[15px] min-h-60 mr-[2px] mb-[2px] overflow-hidden">
+                <img 
+                className="max-w-[180px] m-auto h-auto"
+                src="src\assets\MK-Plastics-300x300.png" alt="" />
             </div>
             <div className="bg-[#090909] w-[367px] flex text-center items-center p-[15px] min-h-60 mr-[2px] mb-[2px] overflow-hidden">
                 <img 
                 className="max-w-[180px] m-auto h-auto"
-                src="src\assets\moment-300x300.png" alt="" />
+                src="src\assets\JB-Associates-300x300.png" alt="" />
+            </div>
+            <div className="bg-[#090909] w-[367px] flex text-center items-center p-[15px] min-h-60 mr-[2px] mb-[2px] overflow-hidden">
+                <img 
+                className="max-w-[180px] m-auto h-auto"
+                src="src\assets\Kamal-Jain-300x300.png" alt="" />
+            </div>
+            <div className="bg-[#090909] w-[367px] flex text-center items-center p-[15px] min-h-60 mr-[2px] mb-[2px] overflow-hidden">
+                <img 
+                className="max-w-[180px] m-auto h-auto"
+                src="src\assets\ghar-pe-tailor-300x300.png" alt="" />
             </div>
 
           </div>
